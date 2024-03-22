1fed3b5b6e5747ef8381b37258dc8d65e0c7f9d5
KimTaeYoung <m45416820@gmail.com>
2024-03-22 21:40:35 +0900

    feat : 최댓값 구하기 - 자연수 9개 중 최댓값과 그 최댓값이 몇 번째 수인지 구하는 기능 구현

---
+++ 2562.py
@@ -0,0 +1,19 @@
+from typing import Any, Sequence
+
+def maximum_of(a: Sequence) -> Any:
+    maximum = a[0]
+    for i in range(1, len(a)):
+        if a[i] > maximum:
+            maximum = a[i]
+    return maximum
+
+if __name__ == "__main__":
+    x = [None] * 9
+    for i in range(9):
+        x[i] = int(input())
+    
+    max_value = maximum_of(x)
+    max_index = x.index(max_value) + 1
+    
+    print(f"{max_value}")
+    print(f"{max_index}")
