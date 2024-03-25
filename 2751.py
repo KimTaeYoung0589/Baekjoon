d7cb35382cf3ccf2de30a5b0d3eec85852a381c0
KimTaeYoung <m45416820@gmail.com>
2024-03-25 09:51:00 +0900

    feat : 수 정렬하기2 - 수 정렬하기를 시간 복잡도 고려하여 기능 구현

---
+++ 2751.py
@@ -0,0 +1,10 @@
+# 수 정렬하기 2
+import sys
+
+x = int(sys.stdin.readline())
+num_list = []
+for i in range(x):
+    num_list.append(int(sys.stdin.readline()))
+num_list1 = sorted(num_list)
+for i in range(len(num_list)):
+    print(num_list1[i])
