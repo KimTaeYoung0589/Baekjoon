08e024bb7f1010f59b981a040ec10caff4849a2e
KimTaeYoung <m45416820@gmail.com>
2024-03-30 11:33:13 +0900

    feat : 바이러스 - 백준 2606번 바이러스 BFS 사용해서 로직 이해/구현

---
+++ W02/2606.py
@@ -0,0 +1,30 @@
+from collections import deque
+from sys import stdin
+
+nComputer = int(stdin.readline())
+cNetwork = int(stdin.readline())
+
+checkVirus = [[False] * (nComputer + 1) for _ in range(nComputer + 1)]
+
+for _ in range(cNetwork):
+    a, b = map(int, input().split())
+    checkVirus[a][b] = True
+    checkVirus[b][a] = True
+
+visited = [False] * (nComputer + 1)
+
+def BFS(start):
+    q = deque([start])
+    visited[start] = True
+    count = 0
+    while q:
+        start = q.popleft()
+        count += 1
+        for i in range(1, nComputer + 1):
+            if not visited[i] and checkVirus[start][i]:
+                q.append(i)
+                visited[i] = True
+    return count
+
+result = BFS(1) - 1  # 1번 컴퓨터는 바이러스에 감염되므로 제외해야 합니다.
+print(result)
