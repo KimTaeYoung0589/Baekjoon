f33b67759a77faa03b16d6dd33b73f0e1149d763
KimTaeYoung <m45416820@gmail.com>
2024-03-31 17:43:46 +0900

    feat : 꼬마 정민 - 백준 11382 꼬마 정민 이해/구현

---
+++ Self-Study/11382.py
@@ -0,0 +1,10 @@
+# 꼬마 정민
+import sys
+
+# input 선언 x
+num1, num2, num3 = map(int,(sys.stdin.readline().split())) 
+
+# input = sys.stdin.readline
+# num1, num2, num3 = map(int, input().split())
+
+print(num1+num2+num3)
