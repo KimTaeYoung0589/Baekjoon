973f0e983771060df329b5af1975a8fc802451e1
KimTaeYoung <m45416820@gmail.com>
2024-04-09 19:26:53 +0900

    feat : 별 찍기 -2 - 백준 2439  별 찍기 -2 for문 사용해서 로직 이해/구현/최적화 + 다른 풀이

---
+++ Self-Study/반복문/2439.py
@@ -0,0 +1,12 @@
+# 별 찍기 -2
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(1, N + 1):
+    print(" " * (N - i) + "*" * i)
+
+# 다른 풀이
+n = int(input())
+[print(' ' * (n - i) + '*' * i)for i in range(1,n + 1)]
