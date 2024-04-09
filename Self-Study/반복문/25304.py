4f1c61654acf2053dcc283b039f244341d4c24d1
KimTaeYoung <m45416820@gmail.com>
2024-04-09 09:40:45 +0900

    feat : 영수증 - 백준 25304 영수증 for문, if문 사용해서 로직 이해/구현 + 최적화 코드

---
+++ Self-Study/반복문/25304.py
@@ -0,0 +1,24 @@
+# 영수증
+import sys
+input = sys.stdin.readline
+
+sum_price = int(input())
+things_kind = int(input())
+
+sum_things = 0
+for i in range(things_kind):
+    things_price, things_cnt = map(int, input().split())
+    sum_things += things_price * things_cnt
+
+if sum_price == sum_things: print("Yes")
+else: print("No")
+
+
+# 최적화 코드
+# import sys
+# input = sys.stdin.readline
+# total = int(input())
+# for i in range(int(input())):
+#     a,b = map(int,input().split())
+#     total -= a*b
+# print("Yes" if total == 0 else "No")
