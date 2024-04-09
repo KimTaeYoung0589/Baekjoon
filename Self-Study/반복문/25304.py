e50df4e524ceb36aa80d5eaa9fc0981508421688
KimTaeYoung <m45416820@gmail.com>
2024-04-09 09:47:21 +0900

    Style : 코드 포맷 변경 - 최적화 코드 포맷 변경

--- Self-Study/반복문/25304.py
+++ Self-Study/반복문/25304.py
@@ -17,8 +17,10 @@ else: print("No")
 # 최적화 코드
 # import sys
 # input = sys.stdin.readline
+
 # total = int(input())
 # for i in range(int(input())):
 #     a,b = map(int,input().split())
 #     total -= a*b
+
 # print("Yes" if total == 0 else "No")
