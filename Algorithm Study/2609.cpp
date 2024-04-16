863e200c25d9c50b133be4fd928fee7f0f6fbbdb
KimTaeYoung <m45416820@gmail.com>
2024-04-16 19:33:47 +0900

    feat : 최대공약수와 최소공배수 - 백준 2609 유클리드 호제법 사용해서 로직 이해/구현

---
+++ Algorithm Study/2609.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n1, n2;
+int gcd(int a, int b){
+    int c = a % b;
+	while (c != 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b; 
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+int main(){
+	cin >> n1 >> n2;
+	cout << gcd(n1, n2) << "\n" << lcm(n1, n2);
+    return 0;
+}
