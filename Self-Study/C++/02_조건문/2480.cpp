a7e051d5d44da3b45d3986dd296bc4b5ff508aae
KimTaeYoung <m45416820@gmail.com>
2024-04-28 14:21:24 +0900

    feat: 주사위 세개 - 백준 2480 if문 사용해서 로직 이해/구현

---
+++ Self-Study/C++/02_조건문/2480.cpp
@@ -0,0 +1,25 @@
+// 주사위 세개
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a, b, c;
+	cin >> a >> b >> c;
+ 
+	if (a != b && b != c && a != c) {
+		int m;
+		m = max({a, b, c});
+		cout << m * 100;
+	}
+	else if (a == b && a == c) {
+		cout << 10000 + a * 1000;
+	}
+	else if (a == b || a == c) {
+		cout << 1000 + a * 100;
+	}
+	else {
+		cout << 1000 + b * 100;
+	}
+	return 0;
+}
