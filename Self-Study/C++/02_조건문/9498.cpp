9bfc4720fe862ff81656ad318bbf7d112d33b5a4
KimTaeYoung <m45416820@gmail.com>
2024-04-28 11:44:07 +0900

    feat: 시험 성적 - 백준 9498 if문 사용해서 로직 이해/구현

---
+++ Self-Study/C++/02_조건문/9498.cpp
@@ -0,0 +1,25 @@
+// 시험 성적
+#include <iostream>
+using namespace std;
+
+int score, result;
+
+int main(){
+    cin >> score;
+
+   if(score>= 90){
+        cout << 'A';
+   }
+   else if(score >= 80){
+       cout << 'B';
+   }
+   else if(score >= 70){
+       cout << 'C';
+   }
+   else if(score >= 60){
+       cout << 'D';
+   }
+   else{
+       cout << 'F';
+   }
+}
