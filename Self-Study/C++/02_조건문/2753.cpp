bc733e4a69926dc7cdfdec908479a24ad14140af
KimTaeYoung <m45416820@gmail.com>
2024-04-28 12:42:04 +0900

    feat: 윤년 - 백준 2753 if문 사용해서 로직 이해/구현

---
+++ Self-Study/C++/02_조건문/2753.cpp
@@ -0,0 +1,24 @@
+// 윤년
+#include <iostream>
+using namespace std;
+
+int year;
+int main() {
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                cout << "1";
+            } else {
+                cout << "0";
+            }
+        } else {
+            cout << "1";
+        }
+    } else {
+        cout << "0";
+    }
+
+    return 0;
+}
