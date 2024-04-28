438e7aceef862a99190c19a768b2d2d2103d82ae
KimTaeYoung <m45416820@gmail.com>
2024-04-28 14:53:36 +0900

    feat: 구구단 - 백준 2739 for문 사용해서 로직 이해/구현

---
+++ Self-Study/C++/03_반복문/2739.cpp
@@ -0,0 +1,13 @@
+// 구구단
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+    for (int i = 1; i < 10; i++){
+        cout << n << " * " << i << " = " << n * i << '\n';
+    }
+    return 0;
+}
