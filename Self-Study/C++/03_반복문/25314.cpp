6f710dff6bf5b262e2e073b911f146d0ae307b4a
KimTaeYoung <m45416820@gmail.com>
2024-04-28 16:45:27 +0900

    feat: 코딩은 체육과목 입니다 - 백준 25314 for문 사용해서 로직 이해/구현

---
+++ Self-Study/C++/03_반복문/25314.cpp
@@ -0,0 +1,15 @@
+// 코딩은 체육과목 입니다
+#include <iostream>
+using namespace std;
+
+int n, print_number;
+
+int main(){
+    cin >> n;
+    for (int i = 0; i < n / 4; i++){
+        cout << "long ";
+    }
+    cout << "int";
+    
+    return 0;
+}
