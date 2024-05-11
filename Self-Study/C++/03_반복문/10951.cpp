a5694a7d148f1f363f542ceff696d82baea5222c
KimTaeYoung0589 <m45416820@gmail.com>
2024-05-11 13:19:44 +0900

    feat: A+B - 4 - 백준 10951 while문 사용해서 문제 이해/구현

---
+++ Self-Study/C++/03_반복문/10951.cpp
@@ -0,0 +1,19 @@
+// A+B - 4
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+int main(){
+    // cin >> a >> b;
+    
+    // while(scanf("%d %d", &a, &b) != EOF){
+    //     cout << a + b << endl;
+    // }
+
+    while(cin >> a >> b){
+        cout << a + b << '\n';
+    }
+    
+    return 0;
+}
